c967c457704b7b2415b1284d9ce94e00e0fff238
Ganiyu Hassan Taiwo <hassantaiwo554@gmail.com>
2024-09-03 23:44:18 +0100

    DRY applied to JS

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const navToggle = document.querySelector(".mobile-nav-toggle");
 const sections = document.querySelectorAll("section");
 const destinationNav = document.querySelectorAll(".destination__nav");
 const destinationNavLinks = document.querySelectorAll(".nav .nav__link");
+const explore = document.querySelector(".explore");
 const columnImg = document.querySelector(".column img");
 const sliderImg = document.querySelector(".slider--img img");
 const slides = document.querySelectorAll(".slide");
@@ -20,61 +21,62 @@ const techImg = document.querySelector(".tech-img img");
 // DOM
 
 // Images
-const bgImagesDesktop = [
-  "starter-code/assets/home/background-home-desktop.jpg",
-  "starter-code/assets/destination/background-destination-desktop.jpg",
-  "starter-code/assets/crew/background-crew-desktop.jpg",
-  "starter-code/assets/technology/background-technology-desktop.jpg",
-];
+const images = {
+  bgImagesDesktop: [
+    "starter-code/assets/home/background-home-desktop.jpg",
+    "starter-code/assets/destination/background-destination-desktop.jpg",
+    "starter-code/assets/crew/background-crew-desktop.jpg",
+    "starter-code/assets/technology/background-technology-desktop.jpg",
+  ],
 
-const bgImagesTablet = [
-  "starter-code/assets/home/background-home-tablet.jpg",
-  "starter-code/assets/destination/background-destination-tablet.jpg",
-  "starter-code/assets/crew/background-crew-tablet.jpg",
-  "starter-code/assets/technology/background-technology-tablet.jpg",
-];
+  bgImagesTablet: [
+    "starter-code/assets/home/background-home-tablet.jpg",
+    "starter-code/assets/destination/background-destination-tablet.jpg",
+    "starter-code/assets/crew/background-crew-tablet.jpg",
+    "starter-code/assets/technology/background-technology-tablet.jpg",
+  ],
 
-const bgImagesMobile = [
-  "starter-code/assets/home/background-home-mobile.jpg",
-  "starter-code/assets/destination/background-destination-mobile.jpg",
-  "starter-code/assets/crew/background-crew-mobile.jpg",
-  "starter-code/assets/technology/background-technology-mobile.jpg",
-];
+  bgImagesMobile: [
+    "starter-code/assets/home/background-home-mobile.jpg",
+    "starter-code/assets/destination/background-destination-mobile.jpg",
+    "starter-code/assets/crew/background-crew-mobile.jpg",
+    "starter-code/assets/technology/background-technology-mobile.jpg",
+  ],
 
-const destinationImages = [
-  "starter-code/assets/destination/image-moon.png",
-  "starter-code/assets/destination/image-mars.png",
-  "starter-code/assets/destination/image-europa.png",
-  "starter-code/assets/destination/image-titan.png",
-];
+  destinationImages: [
+    "starter-code/assets/destination/image-moon.png",
+    "starter-code/assets/destination/image-mars.png",
+    "starter-code/assets/destination/image-europa.png",
+    "starter-code/assets/destination/image-titan.png",
+  ],
 
-const crewImages = [
-  "starter-code/assets/crew/image-douglas-hurley.png",
-  "starter-code/assets/crew/image-mark-shuttleworth.png",
-  "starter-code/assets/crew/image-victor-glover.png",
-  "starter-code/assets/crew/image-anousheh-ansari.png",
-];
+  crewImages: [
+    "starter-code/assets/crew/image-douglas-hurley.png",
+    "starter-code/assets/crew/image-mark-shuttleworth.png",
+    "starter-code/assets/crew/image-victor-glover.png",
+    "starter-code/assets/crew/image-anousheh-ansari.png",
+  ],
 
-const technologyPortraitImages = [
-  "starter-code/assets/technology/image-launch-vehicle-portrait.jpg",
-  "starter-code/assets/technology/image-spaceport-portrait.jpg",
-  "starter-code/assets/technology/image-space-capsule-portrait.jpg",
-];
+  technologyPortraitImages: [
+    "starter-code/assets/technology/image-launch-vehicle-portrait.jpg",
+    "starter-code/assets/technology/image-spaceport-portrait.jpg",
+    "starter-code/assets/technology/image-space-capsule-portrait.jpg",
+  ],
 
-const technologyLandscapeImages = [
-  "starter-code/assets/technology/image-launch-vehicle-landscape.jpg",
-  "starter-code/assets/technology/image-spaceport-landscape.jpg",
-  "starter-code/assets/technology/image-space-capsule-landscape.jpg",
-];
-// Images
+  technologyLandscapeImages: [
+    "starter-code/assets/technology/image-launch-vehicle-landscape.jpg",
+    "starter-code/assets/technology/image-spaceport-landscape.jpg",
+    "starter-code/assets/technology/image-space-capsule-landscape.jpg",
+  ],
+};
 
 const getBackgroundImages = () => {
   if (window.matchMedia("(max-width: 781px)").matches) {
-    return bgImagesMobile;
+    return images.bgImagesMobile;
   } else if (window.matchMedia("(max-width: 1024px)").matches) {
-    return bgImagesTablet;
+    return images.bgImagesTablet;
   } else {
-    return bgImagesDesktop;
+    return images.bgImagesDesktop;
   }
 };
 
@@ -89,6 +91,31 @@ navToggle.addEventListener("click", () => {
     navToggle.setAttribute("aria-expanded", false);
   }
 });
+
+// getting nav and section function
+const getLinkSection = (index, clicked = null) => {
+  // Remove active state from all links
+  links.forEach((link) => link.classList.remove("link--active"));
+
+  // Add active state to the clicked link
+  links[index].classList.add("link--active");
+
+  // Hide all sections
+  sections.forEach((sect) => (sect.style.display = "none"));
+
+  // Determine the section to activate
+  const sectionClass = clicked
+    ? `section--${clicked.dataset.section}`
+    : "section--2";
+  document.querySelector(`.${sectionClass}`).style.display = "block";
+
+  // Get the appropriate background images array based on screen size
+  const bgImages = getBackgroundImages();
+
+  // Change background image based on the index
+  document.body.style.backgroundImage = `url(${bgImages[index]})`;
+};
+
 // link for section
 navLinks.addEventListener("click", (e) => {
   e.preventDefault();
@@ -100,34 +127,20 @@ navLinks.addEventListener("click", (e) => {
   const index = navItems.indexOf(clicked);
   if (index === -1) return;
 
-  // Remove active state from all links
-  links.forEach((link) => link.classList.remove("link--active"));
-
-  // Add active state to the clicked link
-  links[index].classList.add("link--active");
-
-  // Hide all sections
-  sections.forEach((sect) => (sect.style.display = "none"));
-
-  // Activate the clicked section
-  document.querySelector(`.section--${clicked.dataset.section}`).style.display =
-    "block";
-
-  // Get the appropriate background images array based on screen size
-  const bgImages = getBackgroundImages();
-
-  // Change background image based on the index
-  document.body.style.backgroundImage = `url(${bgImages[index]})`;
+  getLinkSection(index, clicked);
 });
 
 // Update background image on window resize
 window.addEventListener("resize", () => {
   if (window.matchMedia("(max-width: 781px)").matches) {
-    document.querySelector(".tech-img img").src = technologyLandscapeImages[0];
+    document.querySelector(".tech-img img").src =
+      images.technologyLandscapeImages[0];
   } else if (window.matchMedia("(max-width: 1024px)").matches) {
-    document.querySelector(".tech-img img").src = technologyLandscapeImages[0];
+    document.querySelector(".tech-img img").src =
+      images.technologyLandscapeImages[0];
   } else {
-    document.querySelector(".tech-img img").src = technologyPortraitImages[0];
+    document.querySelector(".tech-img img").src =
+      images.technologyPortraitImages[0];
   }
 
   const activeLink = document.querySelector(".nav__link.active");
@@ -138,6 +151,10 @@ window.addEventListener("resize", () => {
     const bgImages = getBackgroundImages();
     document.body.style.backgroundImage = `url(${bgImages[index]})`;
   }
+});
+
+explore.addEventListener("click", () => {
+  getLinkSection(1);
 });
 
 // link for destination
@@ -160,7 +177,7 @@ destinationNavLinks.forEach((item, i) => {
     ).style.display = "block";
 
     // Change column image based on the index
-    columnImg.src = destinationImages[i];
+    columnImg.src = images.destinationImages[i];
   });
 });
 
@@ -197,7 +214,7 @@ const slider = () => {
       activateDot(slide);
       slides.forEach((s) => (s.style.display = "none"));
       document.querySelector(`.slide--${slide}`).style.display = "block";
-      sliderImg.src = crewImages[+slide];
+      sliderImg.src = images.crewImages[+slide];
     }
   });
 };
@@ -205,16 +222,13 @@ slider();
 
 const getScreenSizeImages = () => {
   if (window.matchMedia("(max-width: 780px)").matches) {
-    return technologyLandscapeImages;
+    return images.technologyLandscapeImages;
   } else if (window.matchMedia("(max-width: 1024px)").matches) {
-    return technologyLandscapeImages;
+    return images.technologyLandscapeImages;
   } else {
-    return technologyPortraitImages;
+    return images.technologyPortraitImages;
   }
 };
-//   const images = getScreenSizeImages();
-//   techImg.src = images[index]; // Update this index based on your logic
-// };
 
 buttons.addEventListener("click", (e) => {
   const check = e.target.closest(".btn");
